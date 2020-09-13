807f90568fc35d3e0e7fea55292a082b32bf1554
danielVg <danielveragi@gmail.com>
2020-09-13 21:10:18 +0200

    remove unnecessary comments

--- lib/main.dart
+++ lib/main.dart
@@ -9,21 +9,11 @@ class Website extends StatelessWidget {
 
   static final DateTime MEETING_DATE = DateTime(2020, 9, 19, 18);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Coffee Show',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
