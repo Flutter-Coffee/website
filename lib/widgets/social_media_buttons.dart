626a81c27c29e846b4230fa9909cb2e3adb970ee
danielVg <danielveragi@gmail.com>
2020-09-15 22:01:37 +0200

    change to fafa icons

--- lib/widgets/social_media_buttons.dart
+++ lib/widgets/social_media_buttons.dart
@@ -1,10 +1,11 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 abstract class SocialMediaButton extends StatelessWidget {
   final String tooltip;
-  final Image icon;
+  final FaIcon icon;
   final String websiteLink;
   final String websiteName;
 
@@ -12,62 +13,28 @@ abstract class SocialMediaButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Tooltip(
-      message: tooltip,
-      child: Container(
-        height: 60,
-        width: 60,
-        child: Material(
-          color: Colors.black38,
-          borderRadius: BorderRadius.all(Radius.circular(200)),
-          child: InkWell(
-            onTap: () async {
-              window.open(websiteLink, websiteName);
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: icon,
-            ),
-          ),
-        ),
-      ),
+    return IconButton(
+      tooltip: tooltip,
+      icon: icon,
+      onPressed: () async {
+        window.open(websiteLink, websiteName);
+      },
     );
   }
-
 }
 
 class InstagramButton extends SocialMediaButton {
-  const InstagramButton() : super(
-    tooltip:  "Follow us on Instagram!",
-    icon: const Image(image: AssetImage("instagram.png")),
-    websiteLink: "https://instagram.com/flutter.coffee",
-    websiteName: "Instagram"
-  );
+  const InstagramButton() : super(tooltip: "Follow us on Instagram!", icon: const FaIcon(FontAwesomeIcons.instagram), websiteLink: "https://instagram.com/flutter.coffee", websiteName: "Instagram");
 }
 
 class TwitterButton extends SocialMediaButton {
-  const TwitterButton() : super(
-      tooltip:  "Follow us on Twitter!",
-      icon: const Image(image: AssetImage("twitter.png")),
-      websiteLink: "https://twitter.com/fluttercoffee",
-      websiteName: "Twitter"
-  );
+  const TwitterButton() : super(tooltip: "Follow us on Twitter!", icon: const FaIcon(FontAwesomeIcons.twitter), websiteLink: "https://twitter.com/fluttercoffee", websiteName: "Twitter");
 }
 
 class MeetupButton extends SocialMediaButton {
-  const MeetupButton() : super(
-      tooltip:  "Join the Meetup!",
-      icon: const Image(image: AssetImage("meetup.png")),
-      websiteLink: "http://meetu.ps/e/JjFHN/FZPLN/d",
-      websiteName: "Meetup"
-  );
+  const MeetupButton() : super(tooltip: "Join the Meetup!", icon: const FaIcon(FontAwesomeIcons.meetup), websiteLink: "http://meetu.ps/e/JjFHN/FZPLN/d", websiteName: "Meetup");
 }
 
 class MailButton extends SocialMediaButton {
-  const MailButton() : super(
-      tooltip:  "Send us a Mail!",
-      icon: const Image(image: AssetImage("mail.png")),
-      websiteLink: "mailto:hi@flutter.coffee",
-      websiteName: "Mail"
-  );
+  const MailButton() : super(tooltip: "Send us a Mail!", icon: const FaIcon(FontAwesomeIcons.envelope), websiteLink: "mailto:hi@flutter.coffee", websiteName: "Mail");
 }
